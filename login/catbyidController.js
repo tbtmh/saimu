787970fb5014b92166a6f87fd6f3ec5ea01462f6
tbtmh <ntnch013@gmail.com>
2024-10-01 22:17:34 +0700

    เพิ่ม แยกเครื่องรางกับวัดในหน้าหมวดหมู่ เพิ่มปุ่มย้อนกลับ

--- login/catbyidController.js
+++ login/catbyidController.js
@@ -1,23 +1,21 @@
-require('../models/database');
 const Category = require('../models/Category');
 const Temple = require('../models/Temple');
+const Amulet = require('../models/Amulet');
 
-/**
- * GET /categories/:id
- * Categories By Id
-*/ 
-module.exports = async(req, res) => { 
-    try {
-      let categoryId = req.params.id;
-      const limitNumber = 20;
-      const categoryById = await Temple.find({ 'หมวดหมู่': categoryId }).limit(limitNumber);
-      console.log(categoryById)
-      res.render('categories', { 
-        
-        categoryById } );
+module.exports = async (req, res) => { 
+  try {
+    let categoryId = req.params.id;
+    const limitNumber = 20;
+    
+    // ดึงข้อมูลแยกกันระหว่าง Temple และ Amulet
+    const categoryById = await Temple.find({ 'หมวดหมู่': categoryId }).limit(limitNumber);
+    const categoryByIdamu = await Amulet.find({ 'หมวดหมู่': categoryId }).limit(limitNumber);
 
-    } 
-    catch (error) {
-      res.status(500).send({message: error.message || "Error Occured" });
-    }
+    res.render('categories', { 
+      categoryById: categoryById,
+      categoryByIdamu: categoryByIdamu
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Error Occured" });
   }
+};
