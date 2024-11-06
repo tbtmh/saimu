d79aa748c3b8717e47e8695e016d5a785d3ed5e1
Laliphas <paphas.teera@gmail.com>
2024-11-07 01:41:29 +0700

    fixmodel

--- ai/app.py
+++ ai/app.py
@@ -9,6 +9,23 @@ model = joblib.load('./save_model/model.pkl')
 vectorizer = joblib.load('./save_model/vectorizer.pkl')
 label_encoder = joblib.load('./save_model/label_encoder.pkl')
 
+def get_prediction(text, model, threshold=0.59):
+    question_clean = vectorizer.preprocess(text)
+    question_vectorized = vectorizer.transform(pd.Series([question_clean]))
+
+    try:
+        probabilities = model.predict_proba(question_vectorized)
+        confidence = max(probabilities[0])
+    except AttributeError:
+        confidence = 1 
+
+    predicted_label = model.predict(question_vectorized)[0]
+
+    if confidence < threshold:
+        return "ไม่ระบุหมวดหมู่"
+    else:
+        return label_encoder.inverse_transform([predicted_label])[0]
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
@@ -17,14 +34,9 @@ def predict():
     if not question:
         return jsonify({'error': 'No input provided'}), 400
 
-    question_clean = vectorizer.preprocess(question)  
-    question_vectorized = vectorizer.transform(pd.Series([question_clean]))
+    predicted_category = get_prediction(question, model)
 
-    prediction = model.predict(question_vectorized)
-
-    category = label_encoder.inverse_transform(prediction)
-
-    return jsonify({'predicted_category': category[0]})
+    return jsonify({'predicted_category': predicted_category})
 
 if __name__ == '__main__':
     app.run(debug=True)
